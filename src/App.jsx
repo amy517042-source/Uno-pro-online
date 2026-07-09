cf37d1e7493f292ba630ff862aafa1f1ba899467
amy517042-source <amy517042@gmail.com>
2026-07-09 17:03:39 +0530

    Refactor App component to render Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-700 text-white">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">🎮 UNO Pro Online</h1>
+import Home from "./pages/Home";
 
-        <p className="mt-4 text-xl">
-          Multiplayer game is under development...
-        </p>
-      </div>
-    </div>
-  );
+export default function App() {
+  return <Home />;
 }
