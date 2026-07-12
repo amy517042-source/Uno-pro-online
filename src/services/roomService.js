cc598c262799f5a7fef9650e26af1038e38f5398
amy517042-source <amy517042@gmail.com>
2026-07-12 23:05:04 +0530

    Update roomService.js

--- src/services/roomService.js
+++ src/services/roomService.js
@@ -1,3 +1,22 @@
+import {
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+
+import { db } from "../firebase/firebase";
+
+function generateRoomCode() {
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    code += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return code;
+}
+
 export async function createRoom(hostId) {
   try {
     const roomCode = generateRoomCode();
@@ -19,4 +38,9 @@ export async function createRoom(hostId) {
     console.error("Firestore Error:", error);
     throw error;
   }
+}
+
+export async function roomExists(roomCode) {
+  const snapshot = await getDoc(doc(db, "rooms", roomCode));
+  return snapshot.exists();
 }
