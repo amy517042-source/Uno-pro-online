5fbe3cff8cb8ef4e5f2404b43ee5c878e3d2fdae
amy517042-source <amy517042@gmail.com>
2026-07-29 22:35:30 +0530

    Update wild CenterPile.jsx

--- src/components/CenterPile.jsx
+++ src/components/CenterPile.jsx
@@ -27,16 +27,35 @@ export default function CenterPile({
       <div className="flex flex-col items-center">
         {topCard ? (
           <UnoCard
-            card={topCard}
-            disabled
-          />
+  card={
+    topCard?.color === "Black"
+      ? {
+          ...topCard,
+          color: currentColor || "Black",
+        }
+      : topCard
+  }
+  disabled
+/>
         ) : (
           <div className="w-20 h-32 rounded-xl border-2 border-dashed border-white"></div>
         )}
 
-        <p className="mt-2 text-white font-bold">
-          Color: {currentColor}
-        </p>
+        <p
+  className={`mt-2 text-lg font-bold ${
+    currentColor === "Red"
+      ? "text-red-500"
+      : currentColor === "Blue"
+      ? "text-blue-500"
+      : currentColor === "Green"
+      ? "text-green-400"
+      : currentColor === "Yellow"
+      ? "text-yellow-300"
+      : "text-white"
+  }`}
+>
+  Active Colour: {currentColor}
+</p>
       </div>
 
     </div>
